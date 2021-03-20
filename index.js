c49125bc11e5c9fcaa35fb9fb3a46a24d9c1999b
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-20 14:42:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = ('ODIyMDgzNDU3MTM5NjA1NTI0.YFNG4Q.ZfKAe9-qM4TjeONqvsUA8_EbgJA');
+const prefixcmd = "!";
 
 client.once('ready', () => {
    console.log('félicitation, LEGION à vu le jour!');
 });
 
 client.on("message", message => {
-    if (message.content === "!ping") {
+  if(!message.content.startsWith(prefixcmd) || message.author.bot) return;
+
+  const commandbody = message.content.slice(prefixcmd.lenght);
+  const args = commandbody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (message.content ===  prefixcmd + "ping") {
       message.channel.send("Pong.")
     }
   })
