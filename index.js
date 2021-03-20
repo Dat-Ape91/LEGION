74464561bec23cd96daad285b4abdc8100a3dbcd
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-20 14:56:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on("message", message => {
   if(!message.content.startsWith(prefixcmd)) return;
 
   if (message.content ===  prefixcmd + "cavaetretoutnoir") {
-      message.reply ("TA GUEULE!") 
+      message.reply("TA GUEULE!");
     }
   })
 
