7a2922f4bc0d6c66cf9d18ae0cf53daa5de54598
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-21 14:57:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
 const prefixcmd = "!";
 
 client.once('ready', () => {
@@ -10,6 +12,14 @@ client.once('ready', () => {
 client.on("message", message => {
   if (message.author.bot) return;
 
+  if (message.content.startsWith(prefixcmd + "user")) {
+    message.channel.send (`je suis suis l'utilisateur' ${message.author.tag}`);
+    }
+  
+  if (message.content.startsWith(prefixcmd + "serveur")) {
+    message.channel.send (`je suis sur le serveur ${message.guild.name}`);
+    }
+  
   if (message.content.startsWith(prefixcmd + "mr1")) {
     message.reply ("tu as activé la mission ressources 1, bonne chance!");
     }
