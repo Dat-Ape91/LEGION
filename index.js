f33a3b847ed8a222a6893555d6b7684806bcf646
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-21 15:13:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -10,23 +10,18 @@ client.once('ready', () => {
 });
 
 client.on("message", message => {
-  if (message.author.bot) return;
+  if (!message.content.startsWith(prefixcmd) || message.author.bot) return;
+  const args = message.content.slice(prefixcmd,length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-  if (message.content.startsWith(prefixcmd + "user")) {
-    message.channel.send (`je suis suis l'utilisateur' ${message.author.tag}`);
-    }
-  
-  if (message.content.startsWith(prefixcmd + "serveur")) {
-    message.channel.send (`je suis sur le serveur ${message.guild.name}`);
-    }
-  
-  if (message.content.startsWith(prefixcmd + "mr1")) {
-    message.reply ("tu as activé la mission ressources 1, bonne chance!");
-    }
+  if (command === "user") message.channel.send (`je suis suis l'utilisateur ${message.author.tag}`);
 
-  if (message.content.startsWith(prefixcmd + "cavaetretoutnoir")) {
-    message.reply ("TA GUEULE!");
-    }
+  if (command === "serveur") message.channel.send (`je suis sur le serveur ${message.guild.name}`);
+  
+  if (command === "mr1") message.reply ("tu as activé la mission ressources 1, bonne chance!");
+
+  if (command === "cavaetretoutnoir") message.reply ("TA GUEULE!");
+  
   });
 
 client.login(process.env.TOKEN);
