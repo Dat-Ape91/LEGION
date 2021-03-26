3871eaa22d963a4fe7617cd478a729cde13fc9a4
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-26 11:52:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,13 +11,22 @@ client.on("message", message => {
 
   if (message.content.startsWith(prefixcmd + "cavaetretoutnoir")) {
     message.reply ("**TA GUEULE!**");
-    }
-    if (message.content.startsWith(prefixcmd + "cavaetretoutrouge")) {
-      message.reply ("Comme le vaisseau de *Joh BARD* qui au passage est trop **BADASS!**");
-      }
-    if (message.content.startsWith(`${prefixcmd}mr1`)) {
-        message.reply ("\n**Mission Récolte**\n*Initialisée*\n**50k poussière de ferrite**\nDans l'ombre, nous agissons!");
-          }
+  }
+  if (message.content.startsWith(prefixcmd + "cavaetretoutrouge")) {
+      message.reply ("Comme le vaisseau de *Joh BARD*\nqui au passage,\nest trop **BADASS!**");
+  }
+  if (message.content.startsWith(prefixcmd + "quiveutunmeme")) {
+    message.reply ("Juste un Doigt *Wolf*");
+  }
+  if (message.content.startsWith(prefixcmd + "tuveuxpasunmemedabord")) {
+    message.reply ("Ok *Wolf\nTu es le dieu du meme,\nnous te vénérons.");
+  } 
+  if (message.content.startsWith(prefixcmd + "hellomother")) {
+      message.reply ("*Ripley*, Planète **LV-426-Acheron** à proximitée,\n---*présence de parasites xenomorphes detectée*---\nMerci de vérifier combinaison et soute de ton vaisseau à ton retour.");
+  }
+  if (message.content.startsWith(`${prefixcmd}mr1`)) {
+      message.reply ("\n**Mission Récolte**\n---*Initialisée*---\n**50k poussière de ferrite**\n---*En attente de validation*---\nDans l'ombre, nous agissons!");
+  }
 
 });
 
