66a92d9c4023a6f5117239f77c5403dfbad19d80
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-26 11:11:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ client.on("message", message => {
   if (message.author.bot) return;
 
   if (message.content.startsWith(prefixcmd + "cavaetretoutnoir")) {
-    message.reply ("TA GUEULE!");
+    message.reply ("**TA GUEULE!**");
     }
     if (message.content.startsWith(prefixcmd + "cavaetretoutrouge")) {
       message.reply ("Comme le vaisseau de *Joh BARD* qui au passage est trop **BADASS!**");
       }
     if (message.content.startsWith(`${prefixcmd}mr1`)) {
-        message.reply ("Mission Récolte **50k poussière de ferrite**, Engagée. *Dans l'ombre nous agissons*");
+        message.reply (
+          "**Mission Récolte**\n" /
+          "**50k poussière de ferrite**\n" /
+          "*Mission Initialisée*\n" /
+          "*Dans l'ombre nous agissons*\n"
+          );
         
         }
 
