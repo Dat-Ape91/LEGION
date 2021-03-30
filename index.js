f06243df13e415005cdeb2de2563a7a3c12b26e6
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-30 15:40:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on("message", message => {
   if (message.author.bot) return;
 
   if (message.content.startsWith(`${prefixcmd}mr1`)) {
-     client.channels("822127955370836018").send("\n**Mission Récolte**\n---*Initialisée*---\n**50k poussière de ferrite**\n---*En attente de validation*---\nDans l'ombre, nous agissons!" + message.author.username);
+     client.channels(822127955370836018).send("\n**Mission Récolte**\n---*Initialisée*---\n**50k poussière de ferrite**\n---*En attente de validation*---\nDans l'ombre, nous agissons!" + message.author.username);
   }
 
 });
