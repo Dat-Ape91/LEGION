f63593e92b61a6895e025adbbeeea85006129cda
Dat-Ape91 <80929273+Dat-Ape91@users.noreply.github.com>
2021-03-31 15:35:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ client.once('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-  guild.channels.cache.get('821889905633394691').send("${member} bienvenue au sein du Black Squadron! Dans l'ombre, nous agissons");
+  guild.channels.cache.get('821889905633394691').send(message.reply + "Bienvenue au sein du Black squadron!");
   member.roles.add('823570094461419591');
 
 });
 
 client.on('guildMemberRemove', member => {
-  client.guild.channels.cache.get(`821889905633394691`).send("${member.user.tag} a quitté les Blacks");
+  client.guild.channels.cache.get(`821889905633394691`).send(message.author.tag + "a faiblement lâché les Blacks!");
   member.roles.add('823570094461419591');
 
 });
